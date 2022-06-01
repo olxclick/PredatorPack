f7f720412a4cae3fe6ce79d0d5222f154397508a
João Lázaro <joaolazaro720@gmail.com>
2022-06-01 10:08:12 +0100

    commit

--- bglogin.php
+++ bglogin.php
@@ -51,21 +51,6 @@
 		}
 	}
 ?>
-
-<html>
-	<head>
-		<link rel="stylesheet" href="CSS/log.css">
-		<title>Predator Pack</title>
-		<script src="jquery-3.2.1.min.js"></script>
-
-		
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-
-	</head>
-	<body>
 		<div class="container" id="container">
 			<div class="form-container sign-up-container">
 			<form enctype="multipart/form-data" action="" method="post">
